769925b6625de0e6c5719fb51bd909c3cee676e2
Yaakov Bressler <yaakovbressler@gmail.com>
2020-04-05 21:17:14 -0400

    fixed up the form a whole bunch

--- submitAddData.js
+++ submitAddData.js
@@ -1,5 +1,28 @@
-function submitAddData(){
-  // This function does this!
-  alert("submitted event!")
+function addData(){
 
+  myData = {}
+
+  var form = document.getElementById('add-data-form');
+  for (const x of form){
+    // get the tagname
+    var tagName = x.tagName
+    if (tagName==="INPUT"){
+      var xType = x.type
+      var xId = x.id
+      var value = x.value
+
+      // skip those not valid
+      if (!xId){ continue;}
+
+      // skip non-selected radio buttons
+      if (xType==="radio" & ! x.checked){continue;}
+
+      // otherwise, add
+      myData[xId] = value
+
+    }
+
+  }
+  // Data looks good!
+  console.log(myData)
 }
